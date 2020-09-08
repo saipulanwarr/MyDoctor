8ae59e60e5b66ae3cfb9ef93eb634970c7d84858
saipulanwarr <saipul0994@gmail.com>
2020-09-08 23:13:25 +0700

    add setup router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Splash, GetStarted} from './pages';
 import {NavigationContainer} from '@react-navigation/native';
+
+import Router from './router';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <GetStarted />
+      <Router />
     </NavigationContainer>
-  )
+  );
 };
 
 export default App;
