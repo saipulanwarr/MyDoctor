c18c92441e78f3d204f83c954850d881bc072d6f
saipulanwarr <saipul0994@gmail.com>
2020-09-09 21:48:12 +0700

    add component rated doctor

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -13,30 +13,34 @@ const Doctor = () => {
   return (
     <View style={styles.page}>
       <View style={styles.content}>
-        <HomeProfile />
-        <Text style={styles.welcome}>
-          Mau konsultasi dengan siapa hari ini?
-        </Text>
-        <View style={styles.wrapperScroll}>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <View style={styles.category}>
-              <Gap width={16} />
-              <DoctorCategory />
-              <DoctorCategory />
-              <DoctorCategory />
-              <DoctorCategory />
-              <Gap width={6} />
-            </View>
-          </ScrollView>
-        </View>
-        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
-        <RatedDoctor />
-        <RatedDoctor />
-        <RatedDoctor />
-        <Text style={styles.sectionLabel}>Good News</Text>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Gap height={30} />
+          <HomeProfile />
+          <Text style={styles.welcome}>
+            Mau konsultasi dengan siapa hari ini?
+          </Text>
+          <View style={styles.wrapperScroll}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={styles.category}>
+                <Gap width={16} />
+                <DoctorCategory />
+                <DoctorCategory />
+                <DoctorCategory />
+                <DoctorCategory />
+                <Gap width={6} />
+              </View>
+            </ScrollView>
+          </View>
+          <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+          <RatedDoctor />
+          <RatedDoctor />
+          <RatedDoctor />
+          <Text style={styles.sectionLabel}>Good News</Text>
+          <NewsItem />
+          <NewsItem />
+          <NewsItem />
+          <Gap height={30} />
+        </ScrollView>
       </View>
     </View>
   );
@@ -50,7 +54,6 @@ const styles = StyleSheet.create({
     backgroundColor: colors.secondary,
   },
   content: {
-    paddingVertical: 30,
     paddingHorizontal: 16,
     flex: 1,
     backgroundColor: colors.white,
