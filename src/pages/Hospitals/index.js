0b2e694217ff3b20e60bfbb414655aec38604261
saipulanwarr <saipul0994@gmail.com>
2020-09-09 22:53:48 +0700

    add hospital screen

--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+
+import {ILHospitalBG} from '../../assets';
+import {fonts, colors} from '../../utils';
+import {ListHospital} from '../../components';
 
 const Hospitals = () => {
   return (
-    <View>
-      <Text>Hospital Page</Text>
+    <View style={styles.page}>
+      <ImageBackground source={ILHospitalBG} style={styles.background}>
+        <Text style={styles.title}>Nearby Hospital</Text>
+        <Text style={styles.desc}>3 Tersedia</Text>
+      </ImageBackground>
+      <View style={styles.content}>
+        <ListHospital />
+        <ListHospital />
+        <ListHospital />
+      </View>
     </View>
   );
 };
 
 export default Hospitals;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: colors.secondary,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderRadius: 20,
+    marginTop: -30,
+    paddingTop: 14,
+  },
+  background: {
+    height: 240,
+    paddingTop: 30,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.white,
+    textAlign: 'center',
+  },
+  desc: {
+    fontSize: 14,
+    fontFamily: fonts.primary[300],
+    color: colors.white,
+    marginTop: 6,
+    textAlign: 'center',
+  },
+});
