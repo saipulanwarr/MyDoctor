bb9840816cac4aae0918712e198cb1d592320b0d
saipulanwarr <saipul0994@gmail.com>
2020-09-22 20:31:23 +0700

    add component chat text input

--- src/components/molecules/InputChat/index.js
+++ src/components/molecules/InputChat/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, TextInput} from 'react-native';
+import {colors, fonts} from '../../../utils';
+import {Button} from '../../atoms';
 
 const InputChat = () => {
   return (
-    <View>
-      <Text>Input Chat Item</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.input} placeholder="Tulis pesan untuk saipul" />
+      <Button type="btn-icon-send" />
     </View>
   );
 };
 
 export default InputChat;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: colors.disable,
+    padding: 14,
+    borderRadius: 10,
+    flex: 1,
+    marginRight: 10,
+    fontSize: 14,
+    fontFamily: fonts.primary.normal,
+    maxHeight: 45,
+  },
+});
--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import {Header, ChatItem, InputChat} from '../../components';
-import { fonts, colors } from '../../utils';
+import {fonts, colors} from '../../utils';
 
 const Chatting = () => {
   return (
-    <View>
+    <View style={styles.page}>
       <Header type="dark-profile" title="Saipul Anwar" />
-      <Text style={styles.chatDate}>21, Maret 2020</Text>
-      <ChatItem />
-      <ChatItem />
-      <ChatItem />
-      <ChatItem />
+      <View style={styles.content}>
+        <Text style={styles.chatDate}>21, Maret 2020</Text>
+        <ChatItem />
+        <ChatItem />
+        <ChatItem />
+        <ChatItem />
+      </View>
       <InputChat />
     </View>
   );
@@ -21,11 +23,18 @@ const Chatting = () => {
 export default Chatting;
 
 const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  content: {
+    flex: 1,
+  },
   chatDate: {
     fontSize: 11,
     fontFamily: fonts.primary.normal,
     color: colors.text.secondary,
     marginVertical: 20,
     textAlign: 'center',
-  }
+  },
 });
