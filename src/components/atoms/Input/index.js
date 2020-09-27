9ae50649970fe4e5bc57f918d1e49481e573a799
saipulanwarr <saipul0994@gmail.com>
2020-09-27 21:20:00 +0700

    use useState to store the value of the text input

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -3,7 +3,7 @@ import {StyleSheet, View, TextInput, Text} from 'react-native';
 
 import {colors, fonts} from '../../../utils';
 
-const Input = ({label}) => {
+const Input = ({label, value, onChangeText, secureTextEntry}) => {
   const [border, setBorder] = useState(colors.border);
   const onFocusForm = () => {
     setBorder(colors.tertiary);
@@ -19,6 +19,9 @@ const Input = ({label}) => {
         onFocus={onFocusForm}
         onBlur={onBlurForm}
         style={styles.input(border)}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
       />
     </View>
   );
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, ScrollView} from 'react-native';
 
 import {Header, Input, Button, Gap} from '../../components';
 import {colors} from '../../utils';
 
 const Register = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [profession, setProfession] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.page}>
       <Header onPress={() => navigation.goBack()} title="Daftar Akun" />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.content}>
-          <Input label="Full Name" />
+          <Input
+            label="Full Name"
+            value={fullName}
+            onChangeText={(value) => setFullName(value)}
+          />
           <Gap height={24} />
-          <Input label="Pekerjaan" />
+          <Input
+            label="Pekerjaan"
+            value={profession}
+            onChangeText={(value) => setProfession(value)}
+          />
           <Gap height={24} />
-          <Input label="Email" />
+          <Input
+            label="Email"
+            value={email}
+            onChangeText={(value) => setEmail(value)}
+          />
           <Gap height={24} />
-          <Input label="Password" />
+          <Input
+            label="Password"
+            value={password}
+            onChangeText={(value) => setPassword(value)}
+            secureTextEntry
+          />
           <Gap height={40} />
           <Button
             title="Continue"
