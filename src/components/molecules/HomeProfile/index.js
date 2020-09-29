d506601c5f5f4505e28856fd2292d0406cd628e8
saipulanwarr <saipul0994@gmail.com>
2020-09-29 21:22:01 +0700

    replace profile data on home page with data localstorage

--- src/components/molecules/HomeProfile/index.js
+++ src/components/molecules/HomeProfile/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import {DummyUser} from '../../../assets';
-import {fonts, colors} from '../../../utils';
+import {ILNullPhoto} from '../../../assets';
+import {fonts, colors, getData} from '../../../utils';
 
 const HomeProfile = ({onPress}) => {
+  const [profile, setProfile] = useState({
+    fullName: '',
+    profession: '',
+    photo: ILNullPhoto,
+  });
+
+  useEffect(() => {
+    getData('user').then((res) => {
+      const data = res;
+      data.photo = {uri: res.photo};
+      setProfile(res);
+    });
+  }, []);
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-      <Image source={DummyUser} style={styles.avatar} />
+      <Image source={profile.photo} style={styles.avatar} />
       <View>
-        <Text style={styles.name}>Saipul Anwar</Text>
-        <Text style={styles.profession}>Mobile Developer</Text>
+        <Text style={styles.name}>{profile.fullName}</Text>
+        <Text style={styles.profession}>{profile.profession}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -31,10 +44,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: fonts.primary[600],
     color: colors.text.primary,
+    textTransform: 'capitalize',
   },
   profession: {
     fontSize: 12,
     fontFamily: fonts.primary[600],
     color: colors.text.secondary,
+    textTransform: 'capitalize',
   },
 });
