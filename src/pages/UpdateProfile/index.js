bd849142196420585880579b3cf0a52da9d11e54
saipulanwarr <saipul0994@gmail.com>
2020-09-29 23:32:09 +0700

    update password

--- src/pages/UpdateProfile/index.js
+++ src/pages/UpdateProfile/index.js
@@ -34,9 +34,43 @@ const UpdateProfile = ({navigation}) => {
   };
 
   const update = () => {
+    if (password.length > 0) {
+      if (password.length < 6) {
+        showMessage({
+          message: 'Password kurang dari 6 karakter',
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      } else {
+        updatePassword();
+        updateProfileData();
+        navigation.replace('MainApp');
+      }
+    } else {
+      updateProfileData();
+      navigation.replace('MainApp');
+    }
+  };
+
+  const updatePassword = () => {
+    Fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        user.updatePassword(password).catch((error) => {
+          showMessage({
+            message: error.message,
+            type: 'default',
+            backgroundColor: colors.error,
+            color: colors.white,
+          });
+        });
+      }
+    });
+  };
+
+  const updateProfileData = () => {
     const data = profile;
     data.photo = photoForDB;
-
     Fire.database()
       .ref(`users/${profile.uid}/`)
       .update(data)
@@ -93,7 +127,12 @@ const UpdateProfile = ({navigation}) => {
           <Gap height={24} />
           <Input label="Email" value={profile.email} disable />
           <Gap height={24} />
-          <Input label="Password" value={password} />
+          <Input
+            label="Password"
+            value={password}
+            onChangeText={(value) => setPassword(value)}
+            secureTextEntry
+          />
           <Gap height={40} />
           <Button title="Save Profile" onPress={update} />
         </View>
