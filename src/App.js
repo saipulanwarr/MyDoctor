751e80fcd6c75b9f38e496330ee92241e85bd0c0
saipulanwarr <saipul0994@gmail.com>
2020-10-05 20:40:13 +0700

    added setup redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
 
 import Router from './router';
 import FlashMessage from 'react-native-flash-message';
+import {Loading} from './components';
+import store from './redux/store';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
-    </>
+      {loading && <Loading />}
+    </Provider>
   );
 };
 
